5ad1d84798df6a698fb38cf59cbcf1dd56accc49
Karolina <karolina.laskowska19@gmail.com>
2021-12-10 01:04:48 +0100

    klucz id jako shared_ptr

--- virus_genealogy.h
+++ virus_genealogy.h
@@ -27,3 +27,35 @@ class VirusGenealogy
     private:
         std::unordered_map<typename Virus::id_type, std::unique_ptr<Vertex<Virus>>> graph;
 };
+/*
+template<typename Virus>
+class Vertex
+{
+public:
+  Vertex(Virus::id_type const &stem_id)
+  {
+    virus = std::make_unique<Virus>(stem_id);
+  }
+private:
+  std::unique_ptr<Virus> virus;
+  std::unordered_set<std::shared_ptr<typename Virus::id_type>> children;
+  std::unordered_set<std::shared_ptr<typename Virus::id_type>> parents;
+};
+
+template <typename Virus>
+class VirusGenealogy
+{
+public:
+  VirusGenealogy(Virus::id_type const &stem_id)
+  {
+    gen_stem_id = std::make_shared<typename Virus::id_type>(stem_id);
+    graph.insert({gen_stem_id, std::make_unique<Vertex<Virus>>(stem_id)});
+  }
+
+  Virus::id_type get_stem_id() const { return *gen_stem_id; }
+
+private:
+  std::unordered_map<std::shared_ptr<typename Virus::id_type>, std::unique_ptr<Vertex<Virus>>> graph;
+  std::shared_ptr<typename Virus::id_type> gen_stem_id;
+};
+ */
