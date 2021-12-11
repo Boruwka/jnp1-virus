0cbeeb582e08d66c6397328c4fb2181300172a31
Karolina <karolina.laskowska19@gmail.com>
2021-12-11 15:39:09 +0100

    get_stem_id()

--- virus_genealogy.h
+++ virus_genealogy.h
@@ -22,48 +22,53 @@ remove
 template<typename Virus>
 class Vertex
 {
-    public:
-        std::shared_ptr<Virus> virus;
-        Vertex(Virus::id_type const &stem_id)
-        {
-            virus = std::make_shared<Virus>(stem_id);
-        }
-    private:
-        std::unordered_set<std::shared_ptr<Vertex<Virus>>> children;
-        std::unordered_set<std::shared_ptr<Vertex<Virus>>> parents;
+public:
+    std::shared_ptr<Virus> virus;
+    Vertex(Virus::id_type const &stem_id)
+    {
+        virus = std::make_shared<Virus>(stem_id);
+    }
+private:
+    std::unordered_set<std::shared_ptr<Vertex<Virus>>> children;
+    std::unordered_set<std::shared_ptr<Vertex<Virus>>> parents;
 };
 
 template <typename Virus>
 class VirusGenealogy
 {
-    public:
-        VirusGenealogy(Virus::id_type const &stem_id)
+public:
+    VirusGenealogy(Virus::id_type const &stem_id)
+    {
+        gen_stem_id = stem_id;
+        graph.insert({stem_id, std::make_unique<Vertex<Virus>>(stem_id)});
+    }
+
+    Virus::id_type get_stem_id() const { return gen_stem_id; }
+
+    bool exists(Virus::id_type const &id) const
+    {
+        return graph.contains(id);
+    }
+
+    const Virus& operator[](typename Virus::id_type const &id) const
+    {
+        auto search = this->graph.find(id);
+        if (search != graph.end())
         {
-            graph.insert({stem_id, std::make_unique<Vertex<Virus>>(stem_id)});
+            return *(search->second->virus);
         }
-
-        bool exists(Virus::id_type const &id) const
+        else
         {
-            return graph.contains(id);
+            // tutaj rzucenie wyjątku Virus Not Found, ale jeszcze nwm jak
+            // to miejsce daje warninga przy kompilacji, bo jeszcze nie ma wyjątku
         }
+    }
 
-        const Virus& operator[](typename Virus::id_type const &id) const
-        {
-            auto search = this->graph.find(id);
-            if (search != graph.end())
-            {
-                return *(search->second->virus);
-            }
-            else
-            {
-                // tutaj rzucenie wyjątku Virus Not Found, ale jeszcze nwm jak
-                // to miejsce daje warninga przy kompilacji, bo jeszcze nie ma wyjątku
-            }
-        }
 
-        
-        
 
-    private:
-        std::unordered_map<typename Virus::id_type, std::unique_ptr<Vertex<Virus>>> graph;
+
+private:
+    std::unordered_map<typename Virus::id_type, std::unique_ptr<Vertex<Virus>>> graph;
+    Virus::id_type gen_stem_id;
+
 };
