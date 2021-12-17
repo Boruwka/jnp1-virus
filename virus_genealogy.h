5501daac55e6b13b9cbc819010695bb405f56e4a
Karolina <karolina.laskowska19@gmail.com>
2021-12-17 17:03:36 +0100

    wyrzucenie implementacji poza klase

--- virus_genealogy.h
+++ virus_genealogy.h
@@ -21,32 +21,11 @@ struct TriedToRemoveStemVirus : public std::exception {
 
 template <typename Virus> class VirusGenealogy {
 public:
-    VirusGenealogy(Virus::id_type const &stem_id) {
-        graph.insert({stem_id, std::make_shared<Vertex>(stem_id)});
-        this->stem_id = stem_id;
-    }
+    VirusGenealogy(Virus::id_type const &stem_id);
 
-    ~VirusGenealogy() noexcept {
-        for (auto v : this->graph) {
-            v.second->children.clear();
-            v.second->parents.clear();
-        }
-    }
+    ~VirusGenealogy() noexcept;
 
-    class Vertex {
-    private:
-        std::shared_ptr<Virus> virus;
-
-    public:
-        Vertex(Virus::id_type const &stem_id) {
-            virus = std::make_shared<Virus>(stem_id);
-        }
-
-        std::shared_ptr<Virus> get_virus() { return virus; }
-
-        std::unordered_set<std::shared_ptr<Vertex>> children;
-        std::unordered_set<std::shared_ptr<Vertex>> parents;
-    };
+    class Vertex;
 
     Virus::id_type get_stem_id() const { return stem_id; }
 
@@ -54,155 +33,217 @@ public:
 
     VirusGenealogy &operator=(const VirusGenealogy &) = delete;
 
-    class children_iterator {
-    public:
-        using iterator_category = std::bidirectional_iterator_tag;
-        using difference_type = std::ptrdiff_t;
-        using value_type = Virus;
-        using pointer = std::shared_ptr<Virus>;
-        using reference = const Virus &;
+    class children_iterator;
 
-        children_iterator() = default;
-        explicit children_iterator(typename std::unordered_set<std::shared_ptr<Vertex>>::iterator _it) {
-            it = _it;
-        }
-        reference operator*() const { return *((*it)->get_virus()); }
-        pointer operator->() { return (*it)->get_virus(); }
-        children_iterator &operator++() {
-            it++;
-            return *this;
-        }
-        children_iterator operator++(int) {
-            children_iterator tmp = *this;
-            ++(*this);
-            return tmp;
-        }
-        children_iterator &operator--() {
-            it--;
-            return *this;
-        }
-        children_iterator operator--(int) {
-            children_iterator tmp = *this;
-            --(*this);
-            return tmp;
-        }
-        friend bool operator==(const children_iterator &a, const children_iterator &b) {
-            return a.it == b.it;
-        };
-        friend bool operator!=(const children_iterator &a, const children_iterator &b) {
-            return a.it != b.it;
-        };
+    children_iterator get_children_begin(Virus::id_type const &id) const;
 
-    private:
-        typename std::unordered_set<std::shared_ptr<Vertex>>::iterator it;
-    };
-
-    VirusGenealogy<Virus>::children_iterator
-    get_children_begin(Virus::id_type const &id) const {
-        try {
-            return children_iterator(graph.at(id)->children.begin());
-        } catch (const std::out_of_range &e) {
-            throw VirusNotFound();
-        }
-    }
-
-    VirusGenealogy<Virus>::children_iterator
-    get_children_end(Virus::id_type const &id) const {
-        try {
-            return children_iterator(graph.at(id)->children.end());
-        } catch (const std::out_of_range &e) {
-            throw VirusNotFound();
-        }
-    }
+    children_iterator get_children_end(Virus::id_type const &id) const;
 
     bool exists(Virus::id_type const &id) const { return graph.contains(id); }
 
-    const Virus &operator[](typename Virus::id_type const &id) const {
-        try {
-            return *(this->graph.at(id)->get_virus());
-        } catch (const std::out_of_range &e) {
-            throw VirusNotFound();
-        }
-    }
+    const Virus &operator[](typename Virus::id_type const &id) const;
 
-    std::vector<typename Virus::id_type>
-    get_parents(typename Virus::id_type const &id) const {
-        std::vector<typename Virus::id_type> parents_vector;
-        try {
-            auto v = graph.at(id);
-            for (auto parent : (v->parents)) {
-                parents_vector.push_back((*parent->get_virus()).get_id());
-            }
-            return parents_vector;
-        } catch (const std::out_of_range &e) {
-            throw VirusNotFound();
-        }
-    }
+    std::vector<typename Virus::id_type> get_parents(Virus::id_type const &id) const;
 
-    void create(typename Virus::id_type const &id, Virus::id_type const &parent_id) {
-        if (exists(id)) {
-            throw VirusAlreadyCreated();
-        }
-        if (!exists(parent_id)) {
-            throw VirusNotFound();
-        }
-        graph.insert({id, std::make_shared<Vertex>(id)});
-        connect(id, parent_id);
-    }
+    void create(Virus::id_type const &id, Virus::id_type const &parent_id);
 
-    void create(typename Virus::id_type const &id,
-                std::vector<typename Virus::id_type> const &parent_ids) {
-        if (exists(id)) {
-            throw VirusAlreadyCreated();
-        }
-        for (auto parent_id : parent_ids) {
-            if (!exists(parent_id)) {
-                throw VirusNotFound();
-            }
-        }
-        graph.insert({id, std::make_shared<Vertex>(id)});
-        for (auto parent_id : parent_ids) {
-            connect(id, parent_id);
-        }
-    }
+    void create(Virus::id_type const &id,
+                std::vector<typename Virus::id_type> const &parent_ids);
 
-    void connect(Virus::id_type const &child_id, Virus::id_type const &parent_id) {
-        try {
-            auto child_vertex = graph.at(child_id);
-            auto parent_vertex = graph.at(parent_id);
-            child_vertex->parents.insert(parent_vertex);
-            parent_vertex->children.insert(child_vertex);
-        } catch (const std::out_of_range &e) {
-            throw VirusNotFound();
-        }
-    }
+    void connect(Virus::id_type const &child_id, Virus::id_type const &parent_id);
 
-    void remove(Virus::id_type const &id) {
-        if (id == stem_id) {
-            throw TriedToRemoveStemVirus();
-        }
-        try {
-            remove_single_vertex(this->graph.at(id));
-        } catch (const std::out_of_range &e) {
-            throw VirusNotFound();
-        }
-    }
+    void remove(Virus::id_type const &id);
 
 private:
     std::unordered_map<typename Virus::id_type, std::shared_ptr<Vertex>> graph;
     typename Virus::id_type stem_id;
 
-    void remove_single_vertex(std::shared_ptr<Vertex> vertex) {
-        for (auto parent : (vertex->parents)) {
-            parent->children.erase(vertex);
-        }
-        for (auto child : (vertex->children)) {
-            child->parents.erase(vertex);
-            if (child->parents.size() == 0) {
-                remove_single_vertex(child);
-            }
-        }
-        graph.erase(vertex->get_virus()->get_id());
-    }
+    void remove_single_vertex(std::shared_ptr<Vertex> vertex);
 };
+
+template <typename Virus>
+VirusGenealogy<Virus>::VirusGenealogy(typename Virus::id_type const &stem_id) {
+    graph.insert({stem_id, std::make_shared<Vertex>(stem_id)});
+    this->stem_id = stem_id;
+}
+
+template <typename Virus>
+VirusGenealogy<Virus>::~VirusGenealogy() noexcept {
+for (auto v : this->graph) {
+v.second->children.clear();
+v.second->parents.clear();
+}
+}
+
+template <typename Virus>
+class VirusGenealogy<Virus>::Vertex {
+private:
+    std::shared_ptr<Virus> virus;
+
+public:
+    Vertex(Virus::id_type const &stem_id) {
+        virus = std::make_shared<Virus>(stem_id);
+    }
+
+    std::shared_ptr<Virus> get_virus() { return virus; }
+
+    std::unordered_set<std::shared_ptr<Vertex>> children;
+    std::unordered_set<std::shared_ptr<Vertex>> parents;
+};
+
+template <typename Virus>
+class VirusGenealogy<Virus>::children_iterator {
+public:
+    using iterator_category = std::bidirectional_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = Virus;
+    using pointer = std::shared_ptr<Virus>;
+    using reference = const Virus &;
+
+    children_iterator() = default;
+    explicit children_iterator(typename std::unordered_set<std::shared_ptr<Vertex>>::iterator _it) {
+        it = _it;
+    }
+    reference operator*() const { return *((*it)->get_virus()); }
+    pointer operator->() { return (*it)->get_virus(); }
+    children_iterator &operator++() {
+        it++;
+        return *this;
+    }
+    children_iterator operator++(int) {
+        children_iterator tmp = *this;
+        ++(*this);
+        return tmp;
+    }
+    children_iterator &operator--() {
+        it--;
+        return *this;
+    }
+    children_iterator operator--(int) {
+        children_iterator tmp = *this;
+        --(*this);
+        return tmp;
+    }
+    friend bool operator==(const children_iterator &a, const children_iterator &b) {
+        return a.it == b.it;
+    };
+    friend bool operator!=(const children_iterator &a, const children_iterator &b) {
+        return a.it != b.it;
+    };
+
+private:
+    typename std::unordered_set<std::shared_ptr<Vertex>>::iterator it;
+};
+
+template <typename Virus>
+VirusGenealogy<Virus>::children_iterator
+VirusGenealogy<Virus>::get_children_begin(typename Virus::id_type const &id) const {
+    try {
+        return children_iterator(graph.at(id)->children.begin());
+    } catch (const std::out_of_range &e) {
+        throw VirusNotFound();
+    }
+}
+
+template <typename Virus>
+VirusGenealogy<Virus>::children_iterator
+VirusGenealogy<Virus>::get_children_end(typename Virus::id_type const &id) const {
+    try {
+        return children_iterator(graph.at(id)->children.end());
+    } catch (const std::out_of_range &e) {
+        throw VirusNotFound();
+    }
+}
+
+template <typename Virus>
+const Virus &VirusGenealogy<Virus>::operator[](typename Virus::id_type const &id) const {
+    try {
+        return *(this->graph.at(id)->get_virus());
+    } catch (const std::out_of_range &e) {
+        throw VirusNotFound();
+    }
+}
+
+template <typename Virus>
+std::vector<typename Virus::id_type>
+VirusGenealogy<Virus>::get_parents(typename Virus::id_type const &id) const {
+    std::vector<typename Virus::id_type> parents_vector;
+    try {
+        auto v = graph.at(id);
+        for (auto parent : (v->parents)) {
+            parents_vector.push_back((*parent->get_virus()).get_id());
+        }
+        return parents_vector;
+    } catch (const std::out_of_range &e) {
+        throw VirusNotFound();
+    }
+}
+
+template <typename Virus>
+void VirusGenealogy<Virus>::create(typename Virus::id_type const &id, typename Virus::id_type const &parent_id) {
+    if (exists(id)) {
+        throw VirusAlreadyCreated();
+    }
+    if (!exists(parent_id)) {
+        throw VirusNotFound();
+    }
+    graph.insert({id, std::make_shared<Vertex>(id)});
+    connect(id, parent_id);
+}
+
+template <typename Virus>
+void VirusGenealogy<Virus>::create(typename Virus::id_type const &id,
+                                   std::vector<typename Virus::id_type> const &parent_ids) {
+    if (exists(id)) {
+        throw VirusAlreadyCreated();
+    }
+    for (auto parent_id : parent_ids) {
+        if (!exists(parent_id)) {
+            throw VirusNotFound();
+        }
+    }
+    graph.insert({id, std::make_shared<Vertex>(id)});
+    for (auto parent_id : parent_ids) {
+        connect(id, parent_id);
+    }
+}
+
+template <typename Virus>
+void VirusGenealogy<Virus>::connect(typename Virus::id_type const &child_id, typename Virus::id_type const &parent_id) {
+    try {
+        auto child_vertex = graph.at(child_id);
+        auto parent_vertex = graph.at(parent_id);
+        child_vertex->parents.insert(parent_vertex);
+        parent_vertex->children.insert(child_vertex);
+    } catch (const std::out_of_range &e) {
+        throw VirusNotFound();
+    }
+}
+
+template <typename Virus>
+void VirusGenealogy<Virus>::remove(typename Virus::id_type const &id) {
+    if (id == stem_id) {
+        throw TriedToRemoveStemVirus();
+    }
+    try {
+        remove_single_vertex(this->graph.at(id));
+    } catch (const std::out_of_range &e) {
+        throw VirusNotFound();
+    }
+}
+
+template <typename Virus>
+void VirusGenealogy<Virus>::remove_single_vertex(std::shared_ptr<Vertex> vertex) {
+    for (auto parent : (vertex->parents)) {
+        parent->children.erase(vertex);
+    }
+    for (auto child : (vertex->children)) {
+        child->parents.erase(vertex);
+        if (child->parents.size() == 0) {
+            remove_single_vertex(child);
+        }
+    }
+    graph.erase(vertex->get_virus()->get_id());
+}
+
 #endif
